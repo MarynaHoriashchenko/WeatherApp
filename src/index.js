b48f2094fc6bf9eee6668aee5fd475527ff8edfa
Maryna Horiashchenko <marinka1409@gmail.com>
2022-11-21 23:27:21 +0100

    added html block to js/ build forecast API

--- src/index.js
+++ src/index.js
@@ -24,6 +24,40 @@ function formatDate(timestamp) {
   return `${day}, ${number}/${month}/${year}  ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forcastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forcastHTML =
+      forcastHTML +
+      `<div class="col-2">
+      <div class="dot">
+        <div class="weather-forecast-date">
+          <div id="weatherForecastDayOfWeek">${day}</div>
+          <div id="weatherForecastDay">15</div>
+        </div>
+      </div>
+        <div class="weather-forecast-temperature">7°</div>
+          <img src="emoji/03d.png" alt="" width="100"/>
+        </div>`;
+  });
+
+  forcastHTML = forcastHTML + `</div>`;
+  forecastElement.innerHTML = forcastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let units = "metric";
+  let apiKey = "3c74t8243co3ea66d406ab662df24665";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&key=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let currentTemperature = document.querySelector("#temperatureToday");
   let city = document.querySelector(".choise-city");
@@ -43,6 +77,8 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src", `emoji/${response.data.weather[0].icon}.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
